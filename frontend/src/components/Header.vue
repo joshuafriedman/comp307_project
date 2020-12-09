4554bd8c7c2214970cd5957c1195497784232c5d
ChloeRed <48622771+ChloeRed@users.noreply.github.com>
2020-12-08 19:36:12 -0500

    Finished Header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,21 +3,85 @@
     <img src="../assets/logo-header.png" alt="CS Logo">
     <div id='searchbar'> 
       <form id="search-form" v-on:submit.prevent="search">
-      <input id='search-bar' type="text"  placeholder="Search"/> 
+      <input id='search-bar' type="text" v-model="query" placeholder="Search"/> 
       <button id="search-submit" type="submit" >
         <img id='search-icon' src="../assets/search-icon.png" alt="">
-    </button>
-</form>
+     </button>
+    </form>
     </div>
-    <div id="menu">
-      <router-link to="/"> Home </router-link>
-      <router-link to="/classes"> Classes </router-link>
-      <router-link to="/people"> People </router-link>
-      <router-link to="/news"> News </router-link>
-      <router-link to="/general-info"> General Info </router-link>
-      <router-link to="/login"> {{ log_in_out }} </router-link>
-      <router-link to="/new-page" v-if="show_new_page"> New Page </router-link>
-    </div>
+
+    <nav role="navigation">
+      <ul>
+        <li id = "top"><a href="/">Home</a></li>
+        <li id = "top"><a href="#">Prospective</a>
+          <ul id="dropdown">
+            <li id= "item"><router-link to="/general-info">General Info</router-link></li>
+            <li id= "item"><a href="#">Why Computer Science?</a></li>
+            <li id= "item"><a href="#">CEGEP</a></li>
+            <li id= "item"><a href="#">Freshmen</a></li>
+            <li id= "item"><a href="#">Choosing a Major</a></li>
+            <li id= "item"><a href="#">Combining Studies</a></li>
+            <li id= "item"><a href="#">Internships</a></li>
+            <li id= "item"><a href="#">Undergrads</a></li>
+            <li id= "item"><a href="#">Transfer Students</a></li>
+          </ul>
+        </li>
+        <li id = "top"><a href="#">Academic</a>
+           <ul id="dropdown">
+            <li id= "item"><a href="#">Undergraduate</a></li>
+            <li id= "item"><a href="#">Graduate</a></li>
+            <li id= "item"><router-link to="/classes">Classes</router-link></li>
+            <li id= "item"><a href="#">Teaching Assistants</a></li>
+            <li id= "item"><a href="#">Funding</a></li>
+          </ul>
+        </li>
+        <li id = "top"><a href="#">Research</a>
+          <ul id="dropdown">
+              <li id= "item"><a href="#">Areas</a></li>
+              <li id= "item"><a href="#">Tech Reports</a></li>
+            </ul>
+        </li>
+        <li id = "top"><a href="#">People</a>
+          <ul id="dropdown">
+              <li id= "item"><router-link to="/people">Faculty</router-link></li>
+              <li id= "item"><a href="#">Staff</a></li>
+              <li id= "item"><a href="#">Community</a></li>
+            </ul>
+        </li>
+        <li id = "top"><a href="#">News</a>
+        <ul id="dropdown">
+              <li id= "item"><router-link to="/news">All</router-link></li>
+              <li id= "item"><a href="#">Announcements</a></li>
+              <li id= "item"><a href="#">Awards</a></li>
+            </ul>
+        </li>
+        <li id = "top"><a href="#">Employment</a>
+          <ul id="dropdown">
+              <li id= "item"><a href="#">Faculty</a></li>
+              <li id= "item"><a href="#">Course Lecturer</a></li>
+              <li id= "item"><a href="#">Priority Points</a></li> <hr>
+              <li id= "item"><a href="#">Research</a></li>
+              <li id= "item"><a href="#">Student</a></li>
+            </ul>
+        </li>
+        <li id = "top"><a href="#">Donate</a></li>
+        <li id = "top"><a href="#">About</a>
+           <ul id="dropdown">
+              <li id= "item"><a href="#">Contacts</a></li>
+              <li id= "item"><a href="#">Facilities</a></li> <hr>
+              <li id= "item"><a href="#">Visitor Form </a></li>
+              <li id= "item"><a href="#">Room Reservations</a></li>
+              <li id= "item"><a href="#">Webmail</a></li> <hr>
+              <li id= "item"><a href="#">Internal</a></li> <hr>
+              <li id= "item"><a href="#">Tech Support</a></li>
+              <li id= "item"><a href="#">Admin Info</a></li>
+            </ul>
+        </li>
+        <li id = "top"> <router-link to="/login"> {{ log_in_out }} </router-link> </li>
+        <li id = "top"> <router-link to="/new-page" v-if="show_new_page"> New Page </router-link> </li>
+      </ul>
+   </nav>
+
   </div>
 </template>
 
@@ -44,33 +108,80 @@ export default {
       this.log_in_out = !val? "Login": "Logout"
     }
   }
-
 };
 </script>
 
 <style scoped>
+a {
+  text-decoration: none;
+}
+
+nav {
+  left: 18%;
+  position: relative;
+}
+
+ul {
+	list-style: none;
+	margin: 0;
+	padding-left: 0;
+}
+
+#top {
+	color: #fff;
+  border: 1px solid white;
+	display: block;
+	float: left;
+  padding: 10px;
+  margin: 5px;
+	position: relative;
+	text-decoration: none;
+  transition-duration: 0.5s;
+}
+  
+#top a {
+  color: white;
+}
+
+li:hover {
+	background: var(--mred);
+}
+
+#dropdown {
+  color: black;
+	visibility: hidden;
+  opacity: 0;
+	position: absolute;
+  transition: all 0.5s ease;
+	left: 0;
+  display: none;
+  text-align: left;
+}
+#dropdown a{
+  color: black;
+}
+
+#top:hover > ul,
+ul li ul:hover {
+  background-color: var(--dutch-white);
+  visibility: visible;
+  opacity:1; 
+  display: block;
+  margin-top: 11px;;
+  
+}
+
+#item {
+	clear: both;
+  width: 200px;
+  color: black;
+  height: 30px;
+  padding-left: 10px;
+}
+
 #main {
   height: 150px;
   background-color: var(--navy);
-  /* position:relative; */
-}
-#menu {
-  position: relative;
-  top: 40px;
-  color: white;
-}
-#menu a {
-  margin: 5px;
-  padding:10px;
-  border: white 1px solid;
-  text-decoration: none;
-  width: 100px;
-  max-width: 100px;
-  color: inherit;
-}
-#menu a:hover {
-  text-shadow: 0px 0px 1px black;
-  text-decoration: none;
 }
 #searchbar{
    /* position: absolute;
